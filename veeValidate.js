4f1464b280329d6c18430dacf9e43c6a639f3b98
Lewis Yuburi <lewisyubu@gmail.com>
2018-02-19 11:12:22 -0500

    adding conditional language import

--- veeValidate.js
+++ veeValidate.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VeeValidate, { Validator } from 'vee-validate'
+<% if (options.lang) { %>
 import <%= options.lang %> from 'vee-validate/dist/locale/<%= options.lang %>'
 
 Validator.localize(<%= options.lang %>)
+<% } %>
 
 Vue.use(VeeValidate)

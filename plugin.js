0fb2edfe8c51de10d43cd0a2911da7af0267adbe
Lewis Yuburi <lewisyubu@gmail.com>
2018-02-19 12:05:27 -0500

    fixed localization settings

--- plugin.js
+++ plugin.js
@@ -3,7 +3,6 @@ import VeeValidate, { Validator } from 'vee-validate'
 <% if (options.lang) { %>
 import <%= options.lang %> from 'vee-validate/dist/locale/<%= options.lang %>'
 
-Validator.localize(<%= options.lang %>)
+Validator.localize('<%= options.lang %>', <%= options.lang %>)
 <% } %>
-
 Vue.use(VeeValidate, <%= JSON.stringify(options.nuxtValidateOptions, null, 2) %>)

e1affa4edec464677ea5f946c96f413111c04b97
Lewis Yuburi <lewisyubu@gmail.com>
2018-02-19 11:07:36 -0500

    adding localization

--- veeValidate.js
+++ veeValidate.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+import <%= options.lang %> from 'vee-validate/dist/locale/<%= options.lang %>'
+
+Validator.localize(<%= options.lang %>)
 
 Vue.use(VeeValidate)

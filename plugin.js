880f14012cb143b590a7d7a5e53b38ed2910e581
Sebastian Thomas <st@seybsen.de>
2019-03-04 12:47:39 +0100

    fix: `lang` option

--- plugin.js
+++ plugin.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import VeeValidate, { Validator } from 'vee-validate'
 <% if (options.lang) { %>
 import <%= options.lang %> from 'vee-validate/dist/locale/<%= options.lang %>'
-
-Validator.localize('<%= options.lang %>', <%= options.lang %>)
 <% } %>
 Vue.use(VeeValidate, <%= JSON.stringify(options.nuxtValidateOptions, null, 2) %>)
+<% if (options.lang) { %>
+Validator.localize('<%= options.lang %>', <%= options.lang %>)
+<% } %>

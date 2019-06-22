539a36158dc4044646e82095381ac1e7b80d369f
Haruaki OTAKE <aaharu@hotmail.com>
2019-06-22 22:01:59 +0900

    integration with nuxt-i18n

--- plugin.js
+++ plugin.js
@@ -10,4 +10,23 @@ Validator.localize('<%= options.lang %>', <%= options.lang %>)
 
 export default ({ app }, inject) => {
   app.validator = Validator
+
+  <% if (options.nuxti18n) { %>
+  const nuxti18n = <%= JSON.stringify(options.nuxti18n) %>
+  if (nuxti18n.locale) {
+    const validatorLocale = nuxti18n.locale[app.i18n.locale] ? nuxti18n.locale[app.i18n.locale] : app.i18n.locale
+    if (app.validator.locale !== validatorLocale) {
+      import(`vee-validate/dist/locale/${validatorLocale}`).then(dic => {
+        app.validator.localize(validatorLocale, dic)
+      })
+    }
+
+    app.i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
+      const newValidatorLocale = nuxti18n.locale[newLocale] ? nuxti18n.locale[newLocale] : newLocale
+      import(`vee-validate/dist/locale/${newValidatorLocale}`).then(dic => {
+        app.validator.localize(newValidatorLocale, dic)
+      })
+    };
+  }
+  <% } %>
 }

df7adeb73aac1e20b8c00a18208a8253a756129d
Lew Yuburi <lewisyubu@gmail.com>
2019-06-24 11:10:25 -0500

    new version

--- plugin.js
+++ plugin.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
+
 <% if (options.lang) { %>
 import <%= options.lang %> from 'vee-validate/dist/locale/<%= options.lang %>'
 <% } %>
+
 Vue.use(VeeValidate, <%= JSON.stringify(options.nuxtValidateOptions, null, 2) %>)
+
 <% if (options.lang) { %>
 VeeValidate.Validator.localize('<%= options.lang %>', <%= options.lang %>)
 <% } %>
